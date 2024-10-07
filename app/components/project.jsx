33544b24b4cef9c87bbaeeec961542c4a5105860
zaitsev69 <126803254+zaitsev69@users.noreply.github.com>
2024-10-07 11:59:43 +0200

    improved ui for project

--- app/components/project.jsx
+++ app/components/project.jsx
@@ -1,33 +1,67 @@
-"use client"
-
+"use client";
 import React, { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
-const ProjectCard = ({ title, date, description, url, inView }) => (
+const ProjectCard = ({ title, techno, date, shortDescription, detailedDescription, image, link, inView, index, onImageClick }) => (
   <motion.div
-    className="bg-white w-80 h-80 rounded-lg p-3 cursor-pointer"
-    whileHover={{ scale: 1.05 }}
-    whileTap={{ scale: 0.95 }}
+    className="group bg-white w-full max-w-full mx-auto rounded-lg p-6 cursor-pointer flex justify-between items-center mb-6 transition-all duration-500 overflow-hidden transform"
     initial={{ opacity: 0, y: 20 }}
     animate={inView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
-    transition={{ duration: 0.3 }}
-    onClick={() => window.open(url, '_blank')}
+    transition={{
+      duration: 0.1,
+      ease: "easeInOut",
+    }}
+    whileHover={{ scale: 1.05 }}  
+    onClick={() => window.open(link, '_blank')}  // Redirection au clic sur la carte
   >
-    <div className='flex justify-between'>
-      <Image src="/folder.svg" width={60} height={60} alt="Folder Icon" />
-      <Image src="/github.svg" width={60} height={60} alt="GitHub Icon" />
-    </div>
-    <div className='flex-col'>
-      <h3 className='text-xl font-bold'>{title}</h3>
-      <p className='italic'>{date}</p>
-      <p className='text-lg mt-4'>{description}</p>
-    </div>
+    {/* Section de gauche - Titre, Techno, Date, et descriptions */}
+    <motion.div
+      className="flex flex-col justify-start w-1/2"
+      initial={{ opacity: 0, x: -50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6, ease: "easeInOut", delay: index * 0.1 }}
+    >
+      <h3 className="text-2xl font-bold mb-2">{title}</h3>
+      <h4 className="text-md font-semibold mb-2">{techno}</h4>
+      <p className="italic text-sm mb-4">{date}</p>
+
+      {/* Texte court affiché en haut */}
+      <p>{shortDescription}</p>
+
+      {/* Texte détaillé affiché en dessous en permanence */}
+      <div className="mt-4">
+        <p>{detailedDescription}</p>
+      </div>
+    </motion.div>
+
+    {/* Image à droite, clique pour affichage plein écran */}
+    <motion.div
+      className="w-1/2 h-auto"
+      initial={{ opacity: 0, x: 50 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{ duration: 0.6, ease: "easeInOut", delay: index * 0.1 }}
+    >
+      <Image 
+        src={image} 
+        width={800} 
+        height={600} 
+        alt="Project image" 
+        className="object-cover w-full h-full rounded-lg"
+        onClick={(e) => {
+          e.stopPropagation();  // Empêche la redirection lors du clic sur l'image
+          onImageClick(image);
+        }}
+      />
+    </motion.div>
   </motion.div>
 );
 
 const Project = () => {
   const [inView, setInView] = useState(false);
+  const [isDevTabOpen, setIsDevTabOpen] = useState(true);  // Dev Projects par défaut
+  const [activeCategory, setActiveCategory] = useState('dev'); // État pour la catégorie active
+  const [selectedImage, setSelectedImage] = useState(null);  // Pour gérer l'affichage de l'image plein écran
   const ref = useRef(null);
 
   useEffect(() => {
@@ -49,55 +83,149 @@ const Project = () => {
     };
   }, []);
 
-  const projects = [
-    {
-      title: "Ce site - NextJs",
-      date: "Juillet 2024",
-      description: "Suite à mon ancien portfolio qui avait été créé en HTML/CSS/JS j'ai décidé d'en faire un nouveau. J'ai d'abord réaliser une maquette sur Figma avant de la développer en NextJs.",
-      url: "https://github.com/zaitsev69/portfolio"
-    },
-    {
-      title: "Site vitrine restaurant - NextJS",
-      date: "Juin 2024",
-      description: "Système de réservation en ligne via un formulaire, gestion des menus via un backoffice. Je me suis appuyé sur la bibliothèque React.",
-      url: "https://github.com/zaitsev69/acacia"
-    },
-    {
-      title: "Festival pente en scène - Wordpress",
-      date: "Mai 2024",
-      description: "Site WordPress réalisé dans le cadre du festival Pente en Scène, utilisant Elementor et du JavaScript, basé sur une maquette que j'avais conçue.",
-      url: "https://ariella-lekogoyolla.mds-lyon.yt/"
-    },
-    {
-      title: "Application reporting incident - Laravel",
-      date: "Avril 2024",
-      description: "Entrée de données sur un formulaire, système de validation et d’édition via une interface administrateur sécurisé puis restitution en front.",
-      url: "https://github.com/zaitsev69/FFS"
-    },
+  const projects = {
+    dev: [
+      {
+        title: "Générateur de tablature",
+        techno: "Vue.js",
+        date: "Septembre 2024",
+        shortDescription: "Projet personnel d'un créateur de tablature (partitions) de guitare.",
+        detailedDescription: `
+          Dans le cadre d'un projet personnel, j'ai développé une application web dédiée à la création de tablatures (partitions) pour guitare, en utilisant VueJS pour une interface utilisateur fluide et réactive.
+          Ce générateur permet aux musiciens de composer et d'éditer leurs propres tablatures en toute simplicité. L'application inclut également des fonctionnalités d'exportation.
+        `,
+        image: "/tablature.png",
+        link: "https://tabs-creator-six.vercel.app/",
+      },
+      {
+        title: "Mon portfolio",
+        techno: "Next.js",
+        date: "Juillet 2024",
+        shortDescription: "Mon portfolio personnel créé avec Next.js.",
+        detailedDescription: `
+          Ce portfolio a été conçu dans le cadre de ma formation en développement web. J'ai utilisé Next.js pour créer une expérience utilisateur fluide et rapide. Le site présente mes projets, mes compétences.
+        `,
+        image: "/portfolio.png",
+        link: "https://github.com/zaitsev69/portfolio",
+      },
+      {
+        title: "Site vitrine restaurant",
+        techno: "Next.js",
+        date: "Juin 2024",
+        shortDescription: "Site vitrine pour un restaurant avec un système de réservation.",
+        detailedDescription: `
+          J'ai développé ce site vitrine pour un restaurant en utilisant Next.js, en mettant l'accent sur le design et l'expérience utilisateur. Le site comprend un système de réservation en ligne.
+        `,
+        image: "/acacia.png",
+        link: "https://www.acaciaenprovence.fr/",
+      },
+    ],
+    graphisme: [
+      {
+        title: "Affiche de concert",
+        techno: "Photoshop",
+        date: "Mars 2024",
+        shortDescription: "Création d'une affiche pour un événement musical.",
+        detailedDescription: `
+          Cette affiche a été conçue pour un concert de musique rock, en utilisant Photoshop pour créer un design attractif et impactant. 
+        `,
+        image: "/concert.jpg",
+        link: "https://fakeconcertlink.com",  // Faux lien inventé
+      }
+    ],
+    ui: [
+      {
+        title: "UI Kit pour une application mobile",
+        techno: "Figma",
+        date: "Mai 2024",
+        shortDescription: "Kit d'interface utilisateur pour une application de gestion.",
+        detailedDescription: `
+          Développement d'un UI Kit complet pour une application mobile, incluant des composants interactifs et des éléments de navigation moderne.
+        `,
+        image: "/ui-kit.jpg",
+        link: "https://fakeuikitlink.com",  // Faux lien inventé
+      }
+    ],
+  };
 
-    {
-      title: "Cookie Clicker - HTML/CSS/JS",
-      date: "Avril 2023",
-      description: "Réalisation d'un cookie clicker pour appréhender les concepts clé de JavaScript tel que les boucles, les objets, les fonctions...",
-      url: "https://github.com/zaitsev69/cookieclicker"
-    }
-  ];
+  const toggleCategory = (category) => {
+    setActiveCategory(category);
+  };
 
   return (
-    <div className="mt-64 hidden lg:block text-black" id="projects" ref={ref}>
-      <h2 className="text-3xl font-bold mb-4 text-white">Mes projets</h2>
-      <div className='grid grid-cols-3 gap-x-3 gap-y-6 2xl:gap-6 2xl:grid-cols-4 items-center'>
-        {projects.map((project, index) => (
-          <ProjectCard 
-            key={index}
-            title={project.title}
-            date={project.date}
-            description={project.description}
-            url={project.url}
-            inView={inView}
-          />
-        ))}
+    <div className="mt-64 text-black" id="projects" ref={ref}>
+      {/* Boutons de catégorie */}
+      <div className="text-center mb-8 sticky top-0 z-50 flex gap-4 justify-center">
+        <motion.button
+          className={`text-white px-4 py-2 rounded-lg ${activeCategory === 'dev' ? 'bg-blue-600' : 'bg-gray-400'}`}
+          onClick={() => toggleCategory('dev')}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          Dev Projects
+        </motion.button>
+        <motion.button
+          className={`text-white px-4 py-2 rounded-lg ${activeCategory === 'graphisme' ? 'bg-blue-600' : 'bg-gray-400'}`}
+          onClick={() => toggleCategory('graphisme')}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          Graphisme
+        </motion.button>
+        <motion.button
+          className={`text-white px-4 py-2 rounded-lg ${activeCategory === 'ui' ? 'bg-blue-600' : 'bg-gray-400'}`}
+          onClick={() => toggleCategory('ui')}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          UI Projects
+        </motion.button>
       </div>
+
+      {/* Affichage des projets */}
+      <AnimatePresence>
+        <motion.div
+          className="flex flex-col gap-8"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.4, ease: "easeInOut" }}
+        >
+          {projects[activeCategory].map((project, index) => (
+            <ProjectCard 
+              key={index}
+              title={project.title}
+              techno={project.techno}
+              date={project.date}
+              shortDescription={project.shortDescription}
+              detailedDescription={project.detailedDescription}
+              image={project.image}
+              link={project.link}
+              inView={inView}
+              index={index}
+              onImageClick={setSelectedImage}  // Affichage plein écran de l'image
+            />
+          ))}
+        </motion.div>
+      </AnimatePresence>
+
+      {/* Affichage plein écran de l'image */}
+      {selectedImage && (
+        <motion.div 
+          className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          onClick={() => setSelectedImage(null)}  // Fermer l'image plein écran au clic
+        >
+          <Image 
+            src={selectedImage} 
+            width={1200} 
+            height={900} 
+            alt="Full screen image"
+            className="object-contain"
+          />
+        </motion.div>
+      )}
     </div>
   );
 };
