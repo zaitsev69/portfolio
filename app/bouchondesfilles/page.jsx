8bc0ee5d00cc22829bd7f45f489574d9a56bafd4
zaitsev69 <126803254+zaitsev69@users.noreply.github.com>
2024-10-09 14:57:45 +0200

    fix agaiiiin

--- app/bouchondesfilles/page.jsx
+++ app/bouchondesfilles/page.jsx
@@ -10,7 +10,7 @@ const BouchonDesFilles = () => {
           <div className="w-1/2 pr-8">
             <h1 className='text-white text-5xl bold mb-4'>Bouchon des Filles</h1>
             <p className='text-white'>
-              Réalisation de la charte graphique d'un restaurant lyonnais, Le bouchon des filles.
+              Réalisation de la charte graphique d&apos;un restaurant lyonnais, Le bouchon des filles.
             </p>
           </div>
           
